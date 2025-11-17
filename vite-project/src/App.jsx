9ad80e21982a4b9386dac7538ff396e027129e9f
KeitaKobayashi-a <kobayashi-keita@jreast.co.jp>
2025-11-17 10:06:40 +0900

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,8 @@
+import NavBar from "./components/NavBar";
+import Seats from "./components/Seats";
+export default function App() {
+  return<>
+      <NavBar/>
+      <Seats/>
+      </>
+}
---
+++ vite-project/src/components/Seats.jsx
@@ -0,0 +1,4 @@
+
+export default function Seats({seats, setSeats}) {
+    
+}
