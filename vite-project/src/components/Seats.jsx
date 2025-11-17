d78413db220f567a3da9621cbcf18c66b4df3f34
KeitaKobayashi-a <kobayashi-keita@jreast.co.jp>
2025-11-17 13:35:42 +0900

    button to Div

--- vite-project/src/components/Seats.jsx
+++ vite-project/src/components/Seats.jsx
@@ -1,33 +1,45 @@
-import { Box, Card, CardContent, Typography, Stack, Button } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  Stack,
+} from '@mui/material';
 
 export default function Seats() {
-
   const seatGroups = Array.from({ length: 4 }, (_, g) =>
     Array.from({ length: 4 }, (_, i) => g * 4 + i + 1)
   );
 
-const parseGroup = (txt) => {
-  const groups = ['A', 'B', 'C', 'D'];
-  return groups[txt];
-};
+  const parseGroup = (txt) => {
+    const groups = ['A', 'B', 'C', 'D'];
+    return groups[txt];
+  };
 
   return (
-    <Box sx={{ p: 3, display: "flex", flexWrap: "wrap", gap: 2 }}>
+    <Box sx={{ p: 3, display: 'flex', flexWrap: 'wrap', gap: 2 }}>
       {seatGroups.map((group, index) => (
-        <Card key={index} sx={{ width: "45%", minWidth: 220 }}>
+        <Card key={index} sx={{ width: '45%', minWidth: 220 }}>
           <CardContent>
             <Typography variant="h6"> {parseGroup(index)}</Typography>
 
             <Stack direction="row" spacing={1} mt={2}>
-              <Button variant="outlined" sx={{ flex: 1 }}>{group[0]}</Button>
-              <Button variant="outlined" sx={{ flex: 1 }}>{group[1]}</Button>
+              <Box sx={{ flex: 1, border: '1px solid #00d9ffff', p: 1 }}>
+                {group[0]}
+              </Box>
+              <Box sx={{ flex: 1, border: '1px solid #00d9ffff', p: 1 }}>
+                {group[1]}
+              </Box>
             </Stack>
 
             <Stack direction="row" spacing={1} mt={1}>
-              <Button variant="outlined" sx={{ flex: 1 }}>{group[2]}</Button>
-              <Button variant="outlined" sx={{ flex: 1 }}>{group[3]}</Button>
+              <Box sx={{ flex: 1, border: '1px solid #00d9ffff', p: 1 }}>
+                {group[2]}
+              </Box>
+              <Box sx={{ flex: 1, border: '1px solid #00d9ffff', p: 1 }}>
+                {group[3]}
+              </Box>
             </Stack>
-
           </CardContent>
         </Card>
       ))}
